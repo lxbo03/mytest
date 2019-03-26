0da64deef8e7e83f11883ac55792283734ce1c07
lxbo03 <729495736@qq.com>
2019-03-26 08:40:42 +0800

    修改了第二次

--- demo-test/src/main/java/test.java
+++ demo-test/src/main/java/test.java
@@ -1,11 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("你好,小黑马");
-<<<<<<< HEAD
-        System.out.println("你好,5678");
-=======
-        System.out.println("你好,1234");
+        System.out.println("你好,123");
 
->>>>>>> dev
     }
 }
